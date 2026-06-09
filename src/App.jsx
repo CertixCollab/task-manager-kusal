33f6a592a909d5ad0e2bc4d9782b3fe0fe61469c
Kusal Damsara <kusal2002d@Gmail.com>
2026-06-09 17:07:42 +0530

    feat: Add about page to the navigation using routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from './components/Dashboard'
 import Profile from './components/Profile'
 import Settings from './components/Settings'
 import Registration from './components/Registration'
+import About from './components/About'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
     <Link to="Profile">Profile</Link>
     <Link to="Settings">Settings</Link>
     <Link to="Register">Register</Link>
+    <Link to="About">About</Link>
   </nav>
   <Routes>
     <Route path="/" element={<Home />} />
@@ -23,6 +25,7 @@ function App() {
     <Route path="/Profile" element={<Profile />} />
     <Route path="/Settings" element={<Settings />} />
     <Route path="/Register" element={<Registration />} />
+    <Route path="/About" element={<About />} />
   </Routes>
   </BrowserRouter>
 )
